7757566ea9b88f4b996b5ef3277d89c19a88cbda
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-09-10 18:57:31 -0300

    update: updated requirements and src

---
+++ app_webhook/src/routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from schema import *
+
+router = APIRouter()
+
+
+@router.post('/person')
+def personRoute()
+    
+@router.post('/account')
+def post(event: dict):
+    return 
+
+@router.post('/card')
